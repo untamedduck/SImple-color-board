379ffeec9065689caa36dffc48a55863c9a390cb
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2022-12-15 14:01:55 -0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,48 @@
+let color = "black";
+let click = true;
+
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns= `repeat(${size},1fr)`;
+    board.style.gridTemplateRows = `repeat(${size},1fr)`;
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div")
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+
+function changeSize(input){
+    if(input>=2 || input <= 100){
+        console.log("Type a size between 2 and 100")
+    }
+    populateBoard(input);
+}
+
+function colorSquare() {
+    if (click) {
+        if (color === 'random') {
+            this.style.backgroundColor = `hs1(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
+}
+function changeColor(choice){
+    color = choice;
+}
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.remove());
+}
+
+document.querySelector('body').addEventListener('click',()=>{
+    click =! click;
+})
